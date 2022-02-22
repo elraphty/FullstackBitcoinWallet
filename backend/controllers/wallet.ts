cba1e327370545ef761121c2799122ea0d692b89
elraphty <elraphtyofficial@gmail.com>
2022-02-22 23:05:16 +0100

    successfully generatd private  key

--- backend/controllers/wallet.ts
+++ backend/controllers/wallet.ts
@@ -1,12 +1,30 @@
 import { Request, Response, NextFunction } from "express";
-import { generateMnemonic } from 'bip39';
+import { generateMnemonic, mnemonicToSeed } from 'bip39';
+import BIP32Factory from 'bip32';
+import * as ecc from 'tiny-secp256k1';
+import { networks } from 'bitcoinjs-lib';
 import { responseSuccess } from "../helpers";
+
+const bip32 = BIP32Factory(ecc);
 
 export const generateMnenomic = (req: Request, res: Response, next: NextFunction): void => {
     try {
         const mnemonic = generateMnemonic(256);
 
         responseSuccess(res, 200, 'Successfully generated mnenomic', mnemonic);
+    } catch (err) {
+        next(err);
+    }
+};
+
+export const generatePrivateKey = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    try {
+        const mnemonic: string = req.body.mnemonic;
+
+        const seed = await mnemonicToSeed(mnemonic); 
+        const privateKey = bip32.fromSeed(seed, networks.bitcoin).toBase58();
+
+        responseSuccess(res, 200, 'Successfully generated privateKey', privateKey);
     } catch (err) {
         next(err);
     }
--- backend/routes/api/v1/wallet.ts
+++ backend/routes/api/v1/wallet.ts
@@ -1,8 +1,10 @@
 import express, {Router, Request, Response} from 'express';
-import {generateMnenomic} from '../../../controllers/wallet';
+import {generateMnenomic, generatePrivateKey} from '../../../controllers/wallet';
 
 const router: Router = express.Router();
 
 router.get('/mnenomic', generateMnenomic);
+
+router.post('/privatekey', generatePrivateKey)
 
 export default router;
