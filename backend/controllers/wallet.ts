9967a1feb236c61810cb002d1259cf5d5d1a1304
elraphty <elraphtyofficial@gmail.com>
2022-02-24 11:18:09 +0100

    Successfully generated address

--- backend/controllers/wallet.ts
+++ backend/controllers/wallet.ts
@@ -1,12 +1,15 @@
 import { Request, Response, NextFunction } from "express";
 import { generateMnemonic, mnemonicToSeed } from 'bip39';
-import BIP32Factory from 'bip32';
+import BIP32Factory, { BIP32Interface } from 'bip32';
 import * as ecc from 'tiny-secp256k1';
-import { networks } from 'bitcoinjs-lib';
+import { networks, payments } from 'bitcoinjs-lib';
 import { responseSuccess } from "../helpers";
 
 const bip32 = BIP32Factory(ecc);
 
+const derivationPath = "m/44'/0'/0'/0/0";
+
+// Controller for generating mnenomic seed
 export const generateMnenomic = (req: Request, res: Response, next: NextFunction): void => {
     try {
         const mnemonic = generateMnemonic(256);
@@ -17,14 +20,50 @@ export const generateMnenomic = (req: Request, res: Response, next: NextFunction
     }
 };
 
-export const generatePrivateKey = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+// Controller for generating master private key
+export const generateMasterKeys = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     try {
         const mnemonic: string = req.body.mnemonic;
 
-        const seed = await mnemonicToSeed(mnemonic); 
-        const privateKey = bip32.fromSeed(seed, networks.bitcoin).toBase58();
+        // networks.bitcoin.bip32.public
 
-        responseSuccess(res, 200, 'Successfully generated privateKey', privateKey);
+        const seed = await mnemonicToSeed(mnemonic); 
+        const node = bip32.fromSeed(seed, networks.testnet);
+        const xprv = node.toBase58();
+
+
+        const xpub = node.derivePath(derivationPath).neutered().toBase58();
+
+        const data = {
+            xprv,
+            xpub,
+        }
+
+        responseSuccess(res, 200, 'Successfully generated privateKey', data);
+    } catch (err) {
+        next(err);
+    }
+};
+
+// Controller for generating master private key
+export const generateChildPubKey = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    try {
+        const xpriv: string  = req.body.publickey;
+
+        const node = bip32.fromBase58(xpriv, networks.testnet);
+
+        const child = node.neutered().toBase58();
+
+        const { address } = payments.p2wpkh({
+            pubkey: bip32.fromBase58(xpriv,  networks.testnet).derive(0).derive(1).publicKey,
+        });
+
+        const data = {
+            child,
+            address
+        };
+      
+        responseSuccess(res, 200, 'Successfully generated address', data);
     } catch (err) {
         next(err);
     }
--- backend/routes/api/v1/wallet.ts
+++ backend/routes/api/v1/wallet.ts
@@ -1,10 +1,12 @@
-import express, {Router, Request, Response} from 'express';
-import {generateMnenomic, generatePrivateKey} from '../../../controllers/wallet';
+import express, {Router} from 'express';
+import {generateMnenomic, generateMasterKeys, generateChildPubKey} from '../../../controllers/wallet';
 
 const router: Router = express.Router();
 
 router.get('/mnenomic', generateMnenomic);
 
-router.post('/privatekey', generatePrivateKey)
+router.post('/privatekey', generateMasterKeys)
+
+router.post('/childpubkey', generateChildPubKey)
 
 export default router;
