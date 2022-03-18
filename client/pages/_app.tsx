97713063eedac0be32f07db3b056d5bca83ebce5
elraphty <elraphtyofficial@gmail.com>
2022-03-18 08:16:38 +0100

    login design done

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,5 +1,6 @@
 import '../styles/globals.css'
 import '../styles/style.scss'
+import '../styles/form.scss'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,10 +1,45 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    colors: {
+      gray: colors.gray,
+      red: colors.red,
+      yellow: colors.amber,
+      indigo: colors.indigo,
+      white: colors.white,
+      orange: colors.orange,
+      blue: colors.blue,
+      black: colors.black,
+      green: colors.green,
+      purple: colors.purple,
+      pink: colors.pink,
+    },
+    extend: {
+      colors: {
+        brand: {
+          DEFAULT: '#0747A6',
+          dark: '#043A8B',
+          text: '#04275D',
+        },
+      },
+      fontFamily: {
+        heading: ['circular', 'sans-serif', 'Helvetica', 'Arial'],
+        text: ['circular', '-apple-system', 'sans-serif'],
+      },
+      gridTemplateRows: {
+        7: 'repeat(7, minmax(0, 1fr))',
+        8: 'repeat(18, minmax(0, 1fr))',
+        9: 'repeat(9, minmax(0, 1fr))',
+        10: 'repeat(10, minmax(0, 1fr))',
+        11: 'repeat(11, minmax(0, 1fr))',
+        12: 'repeat(12, minmax(0, 1fr))',
+      },
+    },
   },
   plugins: [],
-}
+};
