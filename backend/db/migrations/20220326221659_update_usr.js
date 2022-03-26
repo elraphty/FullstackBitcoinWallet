d006dbe43fba2f049694f37cdabad3d2f0814d4e
elraphty <elraphtyofficial@gmail.com>
2022-03-26 23:19:16 +0100

    added pub

---
+++ backend/db/migrations/20220326221659_update_usr.js
@@ -0,0 +1,17 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.alterTable('users', (t) => {
+    t.string('pub');
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTable('users');
+};
